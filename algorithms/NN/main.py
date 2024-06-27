c6549ff2629899c1fade18b5590b9df76bff0737
Aw Thura <awthura2016@gmail.com>
2024-06-27 23:37:06 +0200

    NN explainable_AI

--- algorithms/NN/main.py
+++ algorithms/NN/main.py
@@ -4,6 +4,7 @@ from dataloader import load_data
 from algorithm import SimpleNN
 import torch.nn as nn
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
+import shap
 
 def train(model, train_loader, criterion, optimizer, device, epoch):
     model.train()
@@ -48,6 +49,19 @@ def test(model, val_loader, criterion, device):
     print('Confusion Matrix:')
     print(cm)
 
+def feature_importance(model, val_loader, device, feature_names):
+    model.eval()
+    data_iter = iter(val_loader)
+    data, target = next(data_iter)  # Use next(data_iter) instead of data_iter.next()
+    data, target = data.to(device), target.to(device).view(-1, 1)
+    
+    # Explain the model's predictions using SHAP
+    explainer = shap.DeepExplainer(model, data)
+    shap_values = explainer.shap_values(data, check_additivity=False)
+    
+    # Plot SHAP values
+    shap.summary_plot(shap_values, data.cpu().numpy(), feature_names=feature_names, plot_type='bar')
+
 def main():
     # Parameters
     train_filepath = 'datasets/split/supervised_learning_dataset/train.csv'
@@ -60,8 +74,8 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(device)
     
-    # Load data
-    train_loader, val_loader = load_data(train_filepath, val_filepath)
+    # Load data and get feature names
+    train_loader, val_loader, feature_names = load_data(train_filepath, val_filepath)
     
     # Initialize model, loss function, and optimizer
     model = SimpleNN(input_size=input_size).to(device)
@@ -78,6 +92,10 @@ def main():
     
     # Save model weights
     torch.save(model.state_dict(), 'algorithms/NN/weights/model_weights.pth')
+    
+    ## This line is for feature importance. Could be commented out for normal training
+    #Explain the model feature importance using SHAP
+    #feature_importance(model, val_loader, device, feature_names=feature_names)
 
 if __name__ == '__main__':
     main()
